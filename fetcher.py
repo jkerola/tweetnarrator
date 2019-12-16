2c055b1e9474b575e95f096d37f829792951f3dc
Janne K <44394305+jkerola@users.noreply.github.com>
2019-12-16 17:54:32 +0200

    started working on regex

--- fetcher.py
+++ fetcher.py
@@ -1,7 +1,11 @@
 #!usr/bin/python3
 import tweepy
+import re
 import json
+import pyttsx3
 
+credentials = "credentials.json"
+keyword = "Guilty Gear"
 
 def authenticate(fileName):
     "Load credentials for twitter authentication."
@@ -12,20 +16,36 @@ def authenticate(fileName):
     api = tweepy.API(auth)
     return api
 
-def getTweets(api):
+def getTweets(api, keyword):
     "Fetch tweets based on keyword"
-    public_tweets = api.home_timeline(count = 4)
     filtered_tweets = []
-    for tweet in public_tweets:
-        print(tweet.text)
-        #filtered_tweets.append({tweet.user: tweet.text})
-
-        
+    for tweet in api.search(q=keyword, lang="en", rpp=5):
+        text = filterText(tweet.text)
+        print(text)
+        filtered_tweets.append(text)
     return filtered_tweets
 
-credentials = "credentials.json"
+def filterText(text):
+    "filters out links from tweets"
+    ## thank you SO
+    text = re.sub(r'http\S+', '', text)
+    text = re.sub(r'\\n', ",", text)
+    text = re.sub(r'\b@\S', "", text)
+    return text
 
+def speak(text):
+    "Speaks given text out loud."
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+    engine.stop()
+
+def saveTweets(tweets):
+    with open("tweets.json", "w") as tweets_file:
+        json.dump(tweets, tweets_file)
 
 if __name__ == "__main__":
     api = authenticate(credentials)
-    getTweets(api)
+    tweets = getTweets(api, keyword)
+    speak(tweets)
+    saveTweets(tweets)
