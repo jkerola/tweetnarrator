59d9634da5a159cf09f6424eac3f9b3c565d1756
Janne K <44394305+jkerola@users.noreply.github.com>
2019-12-26 18:21:11 +0200

    Created basic loop, works but without word filters etc

--- fetcher.py
+++ fetcher.py
@@ -1,4 +1,5 @@
 #!usr/bin/python3
+from time import sleep
 import tweepy
 import re
 import json
@@ -6,11 +7,13 @@ import pyttsx3
 
 credentials = "credentials.json"
 keyword = "Guilty Gear"
+subsequent_pass = 0
 
 def authenticate(fileName):
     "Load credentials for twitter authentication."
     with open(fileName) as cred_file:
         credentials = json.load(cred_file)
+    cred_file.close()
     auth = tweepy.OAuthHandler(credentials["CONSUMER_KEY"], credentials["CONSUMER_SECRET"])
     auth.set_access_token(credentials["ACCESS_TOKEN"], credentials["ACCESS_SECRET"])
     api = tweepy.API(auth)
@@ -19,9 +22,11 @@ def authenticate(fileName):
 def getTweets(api, keyword):
     "Fetch tweets based on keyword"
     filtered_tweets = []
-    for tweet in api.search(q=keyword, lang="en", rpp=5):
-        text = filterText(tweet.text)
-        print(text)
+    for tweet in api.search(q=keyword, lang="en", count=2, tweet_mode="extended"):
+        text = filterText(tweet.full_text)
+        author = tweet.author.name
+        # using , to create pauses without TTS reading slash n
+        text = "From " + author + ", " + text + ", "
         filtered_tweets.append(text)
     return filtered_tweets
 
@@ -29,23 +34,55 @@ def filterText(text):
     "filters out links from tweets"
     ## thank you SO
     text = re.sub(r'http\S+', '', text)
-    text = re.sub(r'\\n', ",", text)
-    text = re.sub(r'\b@\S', "", text)
+    text = re.sub("\\n", ", ", text)
+    text = re.sub("@\S+", "", text)
     return text
 
-def speak(text):
+def speak():
     "Speaks given text out loud."
     engine = pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
+    with open("tweets.json") as tweets_file:
+        tweets = json.load(tweets_file)
+    tweets_file.close()
+    for line in tweets:
+        engine.say(line)
+        print(line)
+        engine.runAndWait()
     engine.stop()
+    return 1
 
 def saveTweets(tweets):
+    "Saves fetched tweets locally"
     with open("tweets.json", "w") as tweets_file:
         json.dump(tweets, tweets_file)
+        tweets_file.close()
+
+def compareLastTweets(new_tweets):
+    "Compares whether new tweets have been made since last time."
+    with open("tweets.json") as tweets_file:
+        last_tweets = json.load(tweets_file)
+    tweets_file.close()
+    if new_tweets == last_tweets:
+        return 1
+    else:
+        return 0
+
+def main():
+    "Main function that loops"
+    tweets = getTweets(api, keyword)
+    if subsequent_pass:
+        while compareLastTweets(tweets):
+            print("sleeping until new tweets appear...")
+            sleep(30)
+    saveTweets(tweets)
+    if speak():
+        return 1
+    else:
+        return 0
+
 
 if __name__ == "__main__":
     api = authenticate(credentials)
-    tweets = getTweets(api, keyword)
-    speak(tweets)
-    saveTweets(tweets)
+    while main():
+        subsequent_pass = 1
+        continue
--- narrator.py
+++
@@ -1,9 +0,0 @@
-import sys
-import pyttsx3
-
-engine = pyttsx3.init()
-text = sys.argv[1]
-
-engine.say(text)
-engine.runAndWait()
-engine.stop()
